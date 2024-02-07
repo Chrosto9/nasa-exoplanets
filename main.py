4984064710f5f9c847a68369932d1db73aae244b
Michał Chrostowski <mchrostowski99@gmail.com>
2024-02-07 22:24:52 +0100

    add get and limit

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+import requests 
 
 
 api = FastAPI()
 
-
 @api.get("/planets")
-def x():
-    return("hello")
+def planets(limit: int = 10):
+    url = f"https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+top+{limit}+pl_name,hostname,sy_snum,sy_pnum,st_mass+from+ps&format=json"
+    response = requests.get(url)
+    return response.json()
+
+@api.get("/")
+def home():
+    return("Exoplanets API")
