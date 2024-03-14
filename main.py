a5b1f123e4a9555680f93e0c3d85e187dfb7a4cd
Michał Chrostowski <mchrostowski99@gmail.com>
2024-03-14 20:42:47 +0100

    linear regression

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ def planets(limit: int = 10):
     response = requests.get(url)
     return response.json()
 
+@api.get("/plmass")
+def get_data_to_ml(limit: int = 10):
+    url = f"https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+top+{limit}+pl_masse,st_teff+from+ps&format=json"
+    response = requests.get(url)
+    return response.json()
+
 @api.get("/")
 def home():
     return("Exoplanets API")
